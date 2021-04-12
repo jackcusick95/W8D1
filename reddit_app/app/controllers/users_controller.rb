cb50aa16db86c90d90fc62bf12a7e60cfdd703a2
jackcusick95 <jackcusick95@gmail.com>
2021-04-12 11:34:52 -0400

    Finished users Controller

--- reddit_app/app/controllers/users_controller.rb
+++ reddit_app/app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all 
+        render :index
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end 
+
+    def new
+        @user = User.new 
+        render :new 
+    end 
+
+    def create
+        @user = User.create(user_params)
+
+        if @user.save 
+            login!(@user)
+            redirect_to users_url 
+        else 
+            flash[:errors] = @user.errors.full_messages
+            render :new
+        end 
+    end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end 
 
 end 
