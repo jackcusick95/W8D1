2072bf7d9ce6e1692e1006f503aa181ca852f6c1
jackcusick95 <jackcusick95@gmail.com>
2021-04-12 11:56:33 -0400

    completed auth

--- reddit_app/config/routes.rb
+++ reddit_app/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show, :new, :create]
-  resources :session 
+  resources :session, only: [:new, :create, :destroy]
 
 end
